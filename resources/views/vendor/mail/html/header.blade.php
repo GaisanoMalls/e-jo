353561967f12d34a27feb1b0818e2a451a90293a
Sam Sabellano <ssabellano851998@gmail.com>
2025-01-27 21:44:17 +0800

    updated

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{ $message->embed(asset('images/e-jo.png')) }}" class="logo" alt="E-JO">
-@else
 {{ $slot }}
-@endif
 </a>
 </td>
 </tr>
